0f52fa05ca324e5447e9597561a49064e23cfcf2
suph4rd <suph4rd575@gmail.com>
2021-01-05 18:47:27 +0300

    Bot version 1.1

--- handlers/handler.py
+++ handlers/handler.py
@@ -1,6 +1,7 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.markdown import bold, hbold
 
 from database.database import User, Dish
 from initialise import dp
@@ -91,7 +92,8 @@ async def start_handler(message: types.Message):
 async def main_menu_handler(message: types.Message):
     '''User main menu'''
     await message.answer(
-        text="Главное меню",
+        text=hbold("ГЛАВНОЕ МЕНЮ"),
+        parse_mode='HTML',
         reply_markup=menu_keyboard
     )
     await OrderUser.start_order.set()
@@ -108,14 +110,20 @@ async def menu_handler(call: CallbackQuery, state: FSMContext):
 @dp.callback_query_handler(text=["menu", "to_menu"], state=OrderUser.start_order)
 async def menu_handler(call: CallbackQuery):
     '''Select category'''
-    await call.message.edit_text(text="Выберите категорию")
+    await call.message.edit_text(
+        text=hbold("ВЫБЕРИТЕ КАТЕГОРИЮ"),
+        parse_mode='HTML'
+    )
     await call.message.edit_reply_markup(reply_markup=category_keyboard)
 
 
 @dp.callback_query_handler(text=["order_user"], state=OrderUser.start_order)
 async def order_user_handler(call: CallbackQuery, state: FSMContext):
     '''Select category'''
-    await call.message.edit_text(text="Список заказов")
+    await call.message.edit_text(
+        text=hbold("СПИСОК ЗАКАЗОВ"),
+        parse_mode='HTML'
+    )
     data = await state.get_data()
     if data:
         data_set = data.get("data_set")
@@ -127,13 +135,15 @@ async def order_user_handler(call: CallbackQuery, state: FSMContext):
                     callback_data="remove_dish:{0}".format(val)
                 )]])
             )
-        await call.message.answer(text="Подтвердить заказ",
+        await call.message.edit_reply_markup(
                                   reply_markup=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(
                                       text="Подтвердить заказ",
                                       callback_data="accept"
                                   )]]))
-        await call.message.answer(text="Назад", reply_markup=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(
+        await call.message.answer(
             text="Назад",
+            reply_markup=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(
+                text="Назад",
             callback_data="to_menu"
         )]]))
     else:
@@ -162,7 +172,10 @@ async def dish_handler(call: CallbackQuery):
     '''Select dishes'''
     dish = call.data.split(':')[1]
     dishes = Dish.get_dishes(dish)
-    await call.message.edit_text(text="Выберите блюдо")
+    await call.message.edit_text(
+        text=hbold("Выберите блюдо"),
+        parse_mode='HTML'
+    )
     for val in dishes:
         if val.dish_photo:
             await call.message.answer_photo(photo=open(f"./photo/{val.dish_photo}", "rb"))
