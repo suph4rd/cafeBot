5c25f35514e03278c98c144bb0b4cfd81ef4816f
suph4rd <suph4rd575@gmail.com>
2021-01-10 13:27:27 +0300

    admin change users

--- states/states.py
+++ states/states.py
@@ -2,7 +2,6 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
 class Registration(StatesGroup):
-    start = State()
     not_registration = State()
     fio = State()
     phone_number = State()
